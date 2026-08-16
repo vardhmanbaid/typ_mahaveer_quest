bb7ab32f4636479deaa1fdf69388d2f4274b4a95
Vardhman Baid <vardhman@gebra.ai>
2026-08-16 08:05:46 +0530

    Adjust spacing in Quiz component for improved layout consistency

--- src/components/screens/Quiz.jsx
+++ src/components/screens/Quiz.jsx
@@ -187,7 +187,7 @@ export function Quiz({
       : Math.min(100, (index % total) * total);
 
   return (
-    <div className="mx-auto max-w-xl h-screen px-4 py-4 flex flex-col space-y-24 overflow-hidden">
+    <div className="mx-auto max-w-xl h-screen px-4 py-4 flex flex-col space-y-8 overflow-hidden">
       <div>
         {/* top bar */}
         <div className="flex items-center gap-2 flex-shrink-0">
